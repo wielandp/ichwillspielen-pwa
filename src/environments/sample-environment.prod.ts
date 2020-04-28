45fc93f0691cf61087ce83beb347e1ff58b0696f
wielandp <github@wielandpusch.de>
2020-04-28 16:59:52 +0200

    start

---
+++ src/environments/sample-environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "<<secret api key>>",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx",
+    measurementId: "xxx"
+  },
+  basehref: "pwa/",
+};
