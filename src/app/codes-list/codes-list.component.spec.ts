ef8e48066b1ae6ec84de10f3ee38912727270ed1
wielandp <github@wielandpusch.de>
2021-05-31 18:21:57 +0200

    update cli

--- src/app/codes-list/codes-list.component.spec.ts
+++ src/app/codes-list/codes-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CodesListComponent } from './codes-list.component';
 
@@ -6,7 +6,7 @@ describe('CodesListComponent', () => {
   let component: CodesListComponent;
   let fixture: ComponentFixture<CodesListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CodesListComponent ]
     })
--- src/app/mytrans-list/mytrans-list.component.spec.ts
+++ src/app/mytrans-list/mytrans-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MytransListComponent } from './mytrans-list.component';
 
@@ -6,7 +6,7 @@ describe('MytransListComponent', () => {
   let component: MytransListComponent;
   let fixture: ComponentFixture<MytransListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MytransListComponent ]
     })
